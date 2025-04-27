20cf07e7c0b54b0ea5a0e6c6b31712511f40a840
ajoyfuljester <ajoyfuljester@protonmail.com>
2025-04-27 22:08:54 +0200

    add: function addView in Page, sorry for doing so little stuff

--- static/scripts/Page.js
+++ static/scripts/Page.js
@@ -6,7 +6,20 @@ class Page {
 		*
 	*/
 	constructor() {
+
 		/** @type {View[]} array of views, used for updating */
 		this.views = []
+	}
+
+	/**
+		* @param {View} view View that will be added
+	*/
+	addView(view) {
+		for (const elOffset of view.html.querySelector('.view-offsets').children) {
+			// TODO: idk if this is the right way to do it sorry
+			elOffset.addEventListener()
+		}
+
+		this.views.push(view)
 	}
 }
