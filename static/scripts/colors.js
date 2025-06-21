b6b2057dd4899dbb6cb0b44e779c028b881a6c02
ajoyfuljester <ajoyfuljester@protonmail.com>
2025-06-21 22:29:28 +0200

    ... sorry... green square........... sorry..... sorrrrryyyyy

--- static/scripts/colors.js
+++ static/scripts/colors.js
@@ -1,20 +1,28 @@
 /**
-	* @callback colorFuncion
-	* @param {Object} info information about the cell to color
-	* @property {Number?} info.x x position (date)
-	* @property {Number?} info.y y position (habit)
-	* @returns {String} color string
+	* @typedef {Object} colorFunctionArgument object containing arguments for color function
+	* @property {Number} colorFunctionArgument.x number of dates
+	* @property {Number} colorFunctionArgument.y number of habits
 */
-
 
 /**
-	* @param {Number} max maximum value of `value`, minimum is `0`
-	* @param {Number} hue hue of the gradient (0 to 360)
-	* @returns {colorFuncion} css color (hsl)
+	* @callback colorFunction
+	* @param {colorFunctionArgument} object with arguments 
+	* @returns {String[][]} 2d array with css colors
 */
-export function gradient(max, hue) {
-	return ({x}) => {
-		const step = 100 - (Math.round(x / max * 40) + 10)
-		return `hsl(${hue}deg, 100%, ${step}%)`
+
+
+/* @type {colorFunction} */
+export function gradient({x, y}) {
+	const colors = []
+	for (let i of x) {
+		const row = []
+		for (let j of y) {
+			row.push()
+		}
 	}
+	return `hsl(${hue}deg, 100%, ${step}%)`
 }
+
+// TODO: use that funny functions ``````````````
+const HSL = {}
+
