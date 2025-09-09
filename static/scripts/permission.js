e33b6d3b088e63d6a232424b6535d7fba01eb49d
ajoyfuljester <ajoyfuljester@protonmail.com>
2025-09-09 18:42:24 +0200

    feat(client): add basic functions for permissions

--- static/scripts/permission.js
+++ static/scripts/permission.js
@@ -1,4 +1,5 @@
 import * as Utils from "./utils.js";
+import * as HTMLUtils from "./HTMLUtils.js";
 
 async function main() {
 	const name = Utils.extractName()
@@ -20,6 +21,74 @@ async function main() {
 
 	console.log(data)
 
+
+	const elGet = createPemissionGetElement(data)
+
+	document.querySelector("main").appendChild(elGet)
+
+
+}
+
+
+/**
+	* @param {{guests: Object.<string, Number>, owners: Object.<string, Number>}} data array of permissions of the GUEST with `name`
+	* @returns {HTMLDivElement} element with data tables and other stuff
+*/
+function createPemissionGetElement(data) {
+
+	const el = document.createElement('div')
+
+	const elTableGuests = document.createElement("table")
+
+	// TODO: search bar
+	// table... rows... display none...
+	// also a button for deleting? or maybe an input for changing... also maybe a popup for creating/chaning
+
+	for (const [name, accessMode] of Object.entries(data.guests)) {
+		const elRow = document.createElement('tr')
+
+		const elName = document.createElement('td')
+		elName.textContent = name
+		elRow.appendChild(elName)
+
+		const elAccessMode = document.createElement('td')
+		elAccessMode.textContent = accessMode
+		elRow.appendChild(elAccessMode)
+
+
+		elTableGuests.appendChild(elRow)
+
+	}
+
+
+	el.appendChild(elTableGuests)
+
+
+	// TODO: also a search bar
+
+
+	const elTableOwners = document.createElement("table")
+
+	for (const [name, accessMode] of Object.entries(data.guests)) {
+		const elRow = document.createElement('tr')
+
+		const elName = document.createElement('td')
+		elName.textContent = name
+		elRow.appendChild(elName)
+
+		const elAccessMode = document.createElement('td')
+		elAccessMode.textContent = accessMode
+		elRow.appendChild(elAccessMode)
+
+
+		elTableOwners.appendChild(elRow)
+
+	}
+
+	el.appendChild(elTableOwners)
+
+
+	return el
 
 }
 
